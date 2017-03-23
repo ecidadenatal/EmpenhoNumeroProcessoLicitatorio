2438e5e51697d162b5b7b38608e4bab047be47ca
anderson-dbseller <anderson@dbseller.com.br>
2017-03-23 13:53:44 -0300

    versao em producao

---
+++ estrutura/instalacao.sql
@@ -0,0 +1,11 @@
+CREATE TABLE plugins.empempenhoprocessolicitatorio (
+	sequencial integer,
+	empempenho integer,
+	numerorecibolicitacao integer);
+
+CREATE SEQUENCE plugins.empempenhoprocessolicitatorio_sequencial_seq
+INCREMENT 1
+MINVALUE 1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
